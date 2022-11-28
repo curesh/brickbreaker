2afae6db6ebe72611288074b85141ec7e4d137a6
“Chandra <Chandra.b.suresh@gmail.com>
2022-11-28 14:37:13 -0800

    Scrolling background

--- main-scene.js
+++ main-scene.js
@@ -6,7 +6,7 @@ import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Many_Lights_Demo} from "./examples/many-lights-demo.js"
 import {Obj_File_Demo} from "./examples/obj-file-demo.js"
 
-import {Game} from "./game.js"
+import {Game, Base_Scene} from "./game.js"
 // Pull these names into this module's scope for convenience:
 const {
     Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
@@ -24,7 +24,7 @@ const {
 const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
 
 Object.assign(defs,
-            {Transforms_Sandbox}, {Game}, {Scene_To_Texture_Demo}, {Surfaces_Demo}, {Many_Lights_Demo}, {Obj_File_Demo});
+            {Transforms_Sandbox}, {Game}, {Scene_To_Texture_Demo}, {Surfaces_Demo}, {Many_Lights_Demo}, {Obj_File_Demo}, {Base_Scene});
 
 // ******************** End extra step
 
